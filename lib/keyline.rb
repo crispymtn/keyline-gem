fa186a76d51a8a4a9f0d91d7b613b6512ac62c9b
Christian Weyer <caducitynights@gmail.com>
2017-07-31 21:33:45 +0200

    Remove hardcoded test API key and development host

--- lib/keyline.rb
+++ lib/keyline.rb
@@ -4,9 +4,8 @@ require 'keyline/client'
 
 module Keyline
   class << self
-    def client(options = { host: 'http://keyline.dev', token: '8cad2674e47403b75a84890985f027e2ac6e897a69109074a37919bf4e2b48cc' })
-      return @client if defined?(@client)
-      @client = Keyline::Client.new(options)
+    def client(options = {})
+      @client ||= Keyline::Client.new(options)
     end
 
     def orders
--- lib/keyline/version.rb
+++ lib/keyline/version.rb
@@ -1,3 +1,3 @@
 module Keyline
-  VERSION = "0.3.0"
+  VERSION = "0.3.1"
 end

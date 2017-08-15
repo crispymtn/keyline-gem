45b5277a46ee1dca8e8f97467be4cd5baa43ed0b
Benjamin Reitzammer <benjamin@ambestengestern.de>
2017-08-15 23:58:08 +0200

    allow circulation to be set on production path

--- lib/keyline/resources/production_path.rb
+++ lib/keyline/resources/production_path.rb
@@ -8,8 +8,10 @@ module Keyline
     attributes :state, :circulation, :selected_for_production,
       :show_in_offer, :production_costs, :costs_per_unit, :costs_per_thousand, :total,
       :margin, :discount, :discounted_total, :outsourced, :keep_total,
-      :contribution_margin_for_inhouse_costs, :contribution_margin_for_external_costs, :margin_stepping
+      :contribution_margin_for_inhouse_costs, :contribution_margin_for_external_costs, :margin_stepping,
+      :circulation
 
-    writeable_attributes :selected_for_production, :show_in_offer, :total, :margin, :discount, :outsourced, :keep_total, :margin_stepping
+    writeable_attributes :selected_for_production, :show_in_offer, :total, :margin, :discount, :outsourced,
+      :keep_total, :margin_stepping, :circulation
   end
 end

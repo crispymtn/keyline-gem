b7adf5d067f425a87c6b451a5a26915e3028e18a
Benjamin Reitzammer <benjamin@ambestengestern.de>
2017-08-15 14:00:15 +0200

    add more fields to be set on Order

--- lib/keyline/resources/order.rb
+++ lib/keyline/resources/order.rb
@@ -6,8 +6,8 @@ module Keyline
     extend  Writeable::Resource::ClassMethods
 
     path_prefix :sales
-    attributes :reference, :confirmed_at, :state
-    writeable_attributes :confirmed_at, :state
+    attributes :reference, :confirmed_at, :state, :due_at, :custom_references, :custom_offer_text
+    writeable_attributes :confirmed_at, :state, :due_at, :custom_references, :custom_offer_text
     associations :products
   end
 end

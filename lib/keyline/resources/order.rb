a647c80eb847a9e95bb771b76b9f05248e882e2a
Christian Weyer <caducitynights@gmail.com>
2017-10-19 23:07:58 +0200

    Allow customer_id for an order to be set

--- lib/keyline/resources/order.rb
+++ lib/keyline/resources/order.rb
@@ -6,8 +6,8 @@ module Keyline
     extend  Writeable::Resource::ClassMethods
 
     path_prefix :sales
-    attributes :reference, :confirmed_at, :state, :due_at, :custom_references, :custom_offer_text
-    writeable_attributes :confirmed_at, :state, :due_at, :custom_references, :custom_offer_text
+    attributes :reference, :confirmed_at, :state, :due_at, :custom_references, :custom_offer_text, :customer_id
+    writeable_attributes :confirmed_at, :state, :due_at, :custom_references, :custom_offer_text, :customer_id
     associations :products
   end
 end

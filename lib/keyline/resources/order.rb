4c7a43ef348d837aaf9a9f69c91a6606548cc714
Christian Weyer <cw@crispymtn.com>
2018-06-21 21:05:26 +0200

    Retrieve order costs as well

--- lib/keyline/resources/order.rb
+++ lib/keyline/resources/order.rb
@@ -5,8 +5,8 @@ module Keyline
 
     path_prefix :sales
     attributes :business_unit_id, :reference, :custom_references, :custom_offer_text,
-      :due_at, :confirmed_at, :archived_at, :offer_sent_at, :cleared_at, :state,
-      :shipping_costs, :mixed_shipping, :shipping_costs_billing_mode
+      :costs, :due_at, :confirmed_at, :archived_at, :offer_sent_at, :cleared_at,
+      :state, :shipping_costs, :mixed_shipping, :shipping_costs_billing_mode
 
     writeable_attributes :confirmed_at, :state, :due_at, :custom_references, :custom_offer_text, :customer_id
 

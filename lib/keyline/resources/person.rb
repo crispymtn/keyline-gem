51ee3b3603ddef3068a9e3d678d9bea1802f07cb
Benjamin Reitzammer <br@crispymtn.com>
2018-06-29 16:38:33 +0200

    re-add special handling of person resource

--- lib/keyline/resources/person.rb
+++ lib/keyline/resources/person.rb
@@ -10,5 +10,22 @@ module Keyline
 
     writeable_attributes :name, :reference, :email, :debitor_identifier,
       :creditor_identifier, :preferred_locale, :tax_identifier
+
+    def path
+      Array.new.tap do |segments|
+        segments << @parent.path if @parent
+        segments << self.class.path_prefix if !@parent
+        segments << self.name_in_path
+        segments << self.id if self.persisted?
+      end.join('/')
+    end
+
+    # Provides a possiblity for the given resource
+    # to overwrite paths based on HTTP verb
+    alias_method :path_for_index,   :path
+    alias_method :path_for_show,    :path
+    alias_method :path_for_create,  :path
+    alias_method :path_for_update,  :path
+    alias_method :path_for_destroy, :path
   end
 end

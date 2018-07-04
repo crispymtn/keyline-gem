612ab4e7a00bf455b899a18dee44a7420000da05
Benjamin Reitzammer <br@crispymtn.com>
2018-07-05 00:27:09 +0200

    Extend Parcel resource and add find_by_tracking_code method to it

--- lib/keyline/resources/parcel.rb
+++ lib/keyline/resources/parcel.rb
@@ -8,17 +8,28 @@ module Keyline
     attributes :shipment_id, :state, :dimensions, :weight, :label_url, :tracking_url, :tracking_code, :created_at, :updated_at
     writeable_attributes :state, :tracking_code, :tracking_url, :label_url
 
+    def self.find_by_tracking_code(client:, tracking_code:)
+      self.new(
+        client,
+        client.perform_request(:get, self.path + "/find?tracking_code=#{tracking_code}")
+      )
+    end
+
     ## convenience access to nested attributes
     def shipment
-      self.attributes['shipment'].symbolize_keys.except(:address, :carrier)
+      self.attributes['shipment'].symbolize_keys.except(:address, :carrier, :from_address)
     end
 
     def carrier
       self.attributes['shipment']['carrier'].symbolize_keys
     end
 
-    def address
+    def to_address
       self.attributes['shipment']['address'].symbolize_keys
+    end
+
+    def from_address
+      self.attributes['from_address'].symbolize_keys
     end
   end
 end

36ab2a01b61cb6b89f8e537bc1bbcaf6dd73612a
Christian Weyer <cw@crispymtn.com>
2018-07-04 14:21:06 +0200

    Material presets no longer require a name

--- lib/keyline/resources/material_preset.rb
+++ lib/keyline/resources/material_preset.rb
@@ -3,7 +3,7 @@ module Keyline
     include Jeweler::Resource
     include Jeweler::Writeable::Resource
 
-    attributes :material_id, :raw_material_requirement_name
-    writeable_attributes :material_id, :raw_material_requirement_name
+    attributes :material_id
+    writeable_attributes :material_id
   end
 end

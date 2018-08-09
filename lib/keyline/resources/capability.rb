a760076cd809b0b23e0e89b8f876d49257ab8375
Robert Zacherl <rzacherl@impressed.de>
2018-08-09 07:27:17 +0200

    removed writeable attributes from resources Capability and MeansOfProduction, fixed indention

--- lib/keyline/resources/capability.rb
+++ lib/keyline/resources/capability.rb
@@ -1,9 +1,7 @@
 module Keyline
   class Capability
     include Jeweler::Resource
-    include Jeweler::Writeable::Resource
 
     attributes :kind, :inline, :means_of_production_id
-    writeable_attributes :kind, :inline, :means_of_production_id
    end
 end
--- lib/keyline/resources/means_of_production.rb
+++ lib/keyline/resources/means_of_production.rb
@@ -1,13 +1,8 @@
 module Keyline
   class MeansOfProduction
     include Jeweler::Resource
-    include Jeweler::Writeable::Resource
     
     attributes :printery_id, :name, :kind, :gripper_margin, :gripper_position,
-      :minimum_dimensions, :maximum_dimensions, :minimum_grammage, :maximum_grammage,
-      :double_gripper, :material_storage_area_id, :facility_id, :hardware_identifier
-
-    writeable_attributes :printery_id, :name, :kind, :gripper_margin, :gripper_position,
       :minimum_dimensions, :maximum_dimensions, :minimum_grammage, :maximum_grammage,
       :double_gripper, :material_storage_area_id, :facility_id, :hardware_identifier
 

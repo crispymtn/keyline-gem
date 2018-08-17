dd6b55e4d8c9a5e4cdfeb2b57126219b5ab6b26a
Sascha Weidlich <sw@crispymtn.com>
2018-08-17 13:54:09 +0200

    Update production attributes for production_path and task

--- lib/keyline/resources/production/production_path.rb
+++ lib/keyline/resources/production/production_path.rb
@@ -5,8 +5,8 @@ module Keyline::Production
     attributes :state, :circulation, :selected_for_production,
       :show_in_offer, :production_costs, :costs_per_unit, :costs_per_thousand, :total,
       :margin, :discount, :discounted_total, :outsourced, :keep_total,
-      :contribution_margin_for_inhouse_costs, :contribution_margin_for_external_costs, :margin_stepping
+      :contribution_margin_for_inhouse_costs, :contribution_margin_for_external_costs, :margin_stepping, :tasks, :means_of_production, :material_demands
 
-    associations :circulations, :production_flow_modifications, :tasks
+    associations :circulations, :production_flow_modifications
   end
 end
--- lib/keyline/resources/production/task.rb
+++ lib/keyline/resources/production/task.rb
@@ -4,7 +4,7 @@ module Keyline::Production
 
     path_prefix '/production'
 
-    attributes :kind, :phase, :state, :outsourced, :inline, :checkpoint,
+    attributes :kind, :workable_id, :workable_type, :phase, :state, :outsourced, :inline, :checkpoint,
       :duration_for_preparations, :duration_for_execution, :duration_for_cleanup, :duration_for_pause_until_next_task,
       :manufactured_product, :product_processing,
       :minimum_viable_amount_of_manufactured_products, :amount_of_manufactured_products, :amount_of_wasted_manufactured_products

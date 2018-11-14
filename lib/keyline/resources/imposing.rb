3bf61c386613558ae4cee7b7d20e4bdda88f2014
Christian Weyer <cw@crispymtn.com>
2018-11-14 12:10:58 +0100

    Allow default_stock_folding_pattern to be set for an imposing

--- lib/keyline/resources/imposing.rb
+++ lib/keyline/resources/imposing.rb
@@ -3,8 +3,12 @@ module Keyline
     include Jeweler::Resource
     include Jeweler::Writeable::Resource
 
-    attributes :production_path_id, :printing_technique, :maximum_number_of_mups_per_sheet, :imposing_mode, :preferred_printer_id
-    writeable_attributes :printing_technique, :maximum_number_of_mups_per_sheet, :imposing_mode, :preferred_printer_id
+    attributes :production_path_id, :printing_technique, :maximum_number_of_mups_per_sheet,
+      :imposing_mode, :preferred_printer_id, :default_stock_folding_pattern_id
+
+    writeable_attributes :printing_technique, :maximum_number_of_mups_per_sheet,
+      :imposing_mode, :preferred_printer_id, :default_stock_folding_pattern_id
+
     associations :master_signatures, :substrate
   end
 end

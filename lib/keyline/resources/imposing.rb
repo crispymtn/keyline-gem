52695ac71d77ca5d1047c0fb88bf4066264e7c23
Christian Weyer <cw@crispymtn.com>
2018-11-19 23:07:02 +0100

    Allow auto signature generation to be disabled

--- lib/keyline/resources/imposing.rb
+++ lib/keyline/resources/imposing.rb
@@ -4,10 +4,12 @@ module Keyline
     include Jeweler::Writeable::Resource
 
     attributes :production_path_id, :printing_technique, :maximum_number_of_mups_per_sheet,
-      :imposing_mode, :preferred_printer_id, :default_margin, :default_stock_folding_pattern_id
+      :imposing_mode, :preferred_printer_id, :default_margin, :default_stock_folding_pattern_id,
+      :auto_signature_generation
 
     writeable_attributes :printing_technique, :maximum_number_of_mups_per_sheet,
-      :imposing_mode, :preferred_printer_id, :default_margin, :default_stock_folding_pattern_id
+      :imposing_mode, :preferred_printer_id, :default_margin, :default_stock_folding_pattern_id,
+      :auto_signature_generation
 
     associations :master_signatures, :substrate
   end

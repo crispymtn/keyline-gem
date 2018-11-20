848d69fe263ca7bbf83dc2d14af94209b2540d6b
Christian Weyer <cw@crispymtn.com>
2018-11-20 13:38:20 +0100

    Allow folding state and gripper printing to be set for imposings

--- lib/keyline/resources/imposing.rb
+++ lib/keyline/resources/imposing.rb
@@ -5,11 +5,11 @@ module Keyline
 
     attributes :production_path_id, :printing_technique, :maximum_number_of_mups_per_sheet,
       :imposing_mode, :preferred_printer_id, :default_margin, :default_stock_folding_pattern_id,
-      :auto_signature_generation
+      :auto_signature_generation, :disable_folding, :allow_printing_on_gripper
 
     writeable_attributes :printing_technique, :maximum_number_of_mups_per_sheet,
       :imposing_mode, :preferred_printer_id, :default_margin, :default_stock_folding_pattern_id,
-      :auto_signature_generation
+      :auto_signature_generation, :disable_folding, :allow_printing_on_gripper
 
     associations :master_signatures, :substrate
   end

0a3f5d97f89231a9bf7d44c4d325d4bde49c127b
Sascha Weidlich <sascha.weidlich@mac.com>
2018-11-26 16:27:42 +0100

    Re-add substrate to Component and tasks to SelectedProductionPath

--- lib/keyline/resources/production/component.rb
+++ lib/keyline/resources/production/component.rb
@@ -4,6 +4,6 @@ module Keyline::Production
 
     attributes :name, :number_of_pages, :closed_dimensions, :opened_dimensions, :page_arrangement, :front_colors, :back_colors
 
-    associations :finishings, :imposing, :print_data_files, :master_signatures, :sheets
+    associations :finishings, :imposing, :print_data_files, :master_signatures, :sheets, :substrate
   end
 end

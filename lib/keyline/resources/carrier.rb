9e1b4a6fd2f139e08a4c18883ce00dc517a85552
Christian Weyer <cw@crispymtn.com>
2019-01-23 22:43:58 +0100

    Add supported_countries to carriers

--- lib/keyline/resources/carrier.rb
+++ lib/keyline/resources/carrier.rb
@@ -3,7 +3,10 @@ module Keyline
     include Jeweler::Resource
     include Jeweler::Writeable::Resource
 
-    attributes :id, :name, :service, :automated_labels, :bill_individual_parcels, :created_at, :updated_at
-    writeable_attributes :name, :service, :automated_labels, :bill_individual_parcels
+    attributes :name, :service, :automated_labels, :bill_individual_parcels,
+      :supported_countries
+
+    writeable_attributes :name, :service, :automated_labels,
+      :bill_individual_parcels, :supported_countries
   end
 end

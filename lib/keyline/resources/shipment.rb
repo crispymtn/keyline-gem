a91d924a2d1d2c96429a0a20d72b46bcf7aa8566
Christian Weyer <cw@crispymtn.com>
2019-01-23 22:43:49 +0100

    Remove on longer required shipment_address
    The association now uses Jewelers singleton_association mechanism, a duplication of the address class is thus no longer necessary

--- lib/keyline/resources/shipment.rb
+++ lib/keyline/resources/shipment.rb
@@ -6,6 +6,6 @@ module Keyline
     attributes :reference, :carrier_id, :delivery_contact_phone, :delivery_contact_email, :pick_up_at
     writeable_attributes :carrier_id, :delivery_contact_phone, :delivery_contact_email, :pick_up_at
 
-    associations :shipment_address
+    singleton_associations :address
   end
 end

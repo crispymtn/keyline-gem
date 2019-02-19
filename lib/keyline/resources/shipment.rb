87073836d34a45166f2354eea94dbd47b55abe1a
Christian Weyer <cw@crispymtn.com>
2019-02-19 17:54:20 +0100

    Fix a syntax error in shipment.rb

--- lib/keyline/resources/shipment.rb
+++ lib/keyline/resources/shipment.rb
@@ -3,10 +3,10 @@ module Keyline
     include Jeweler::Resource
     include Jeweler::Writeable::Resource
 
-    attributes :reference, :carrier_id, :delivery_contact_phone, :incoterm
+    attributes :reference, :carrier_id, :delivery_contact_phone, :incoterm,
       :delivery_contact_email, :pick_up_at
 
-    writeable_attributes :carrier_id, :delivery_contact_phone, :inco_term
+    writeable_attributes :carrier_id, :delivery_contact_phone, :inco_term,
       :delivery_contact_email, :pick_up_at
 
     singleton_associations :address

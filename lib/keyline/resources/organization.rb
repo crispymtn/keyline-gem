a4089dfab8b9e0db8b8f1ac0f5e89e46b19790e6
Christian Weyer <cw@crispymtn.com>
2019-02-19 19:32:19 +0100

    Allow customers credit limit to be read and written

--- lib/keyline/resources/organization.rb
+++ lib/keyline/resources/organization.rb
@@ -6,10 +6,10 @@ module Keyline
     path_prefix :customer_relations
 
     attributes :name, :reference, :debitor_identifier, :creditor_identifier,
-      :preferred_locale, :tax_identifier, :deleted_at
+      :preferred_locale, :tax_identifier, :credit_limit, :deleted_at
 
     writeable_attributes :name, :reference, :debitor_identifier, :creditor_identifier,
-      :preferred_locale, :tax_identifier
+      :preferred_locale, :tax_identifier, :credit_limit
 
     associations :people, :address_announcements
   end
--- lib/keyline/resources/person.rb
+++ lib/keyline/resources/person.rb
@@ -7,11 +7,11 @@ module Keyline
 
     attributes :salutation, :first_name, :name, :reference, :email, :debitor_identifier,
       :creditor_identifier, :preferred_locale, :tax_identifier,
-      :phone, :fax, :mobile, :deleted_at
+      :phone, :fax, :mobile, :credit_limit, :deleted_at
 
     writeable_attributes :salutation, :first_name, :name, :reference, :email,
       :debitor_identifier, :creditor_identifier, :preferred_locale, :tax_identifier,
-      :phone, :fax, :mobile
+      :phone, :fax, :mobile, :credit_limit
 
     # Provides a possiblity for the given resource
     # to overwrite paths based on HTTP verb

4d0e5f6e0791bc659f8b502dd8b5ff994710f1b1
Christian Weyer <cw@crispymtn.com>
2019-02-19 17:52:32 +0100

    Allow incoterms to be set for a carrier

--- lib/keyline/resources/shipment.rb
+++ lib/keyline/resources/shipment.rb
@@ -3,8 +3,11 @@ module Keyline
     include Jeweler::Resource
     include Jeweler::Writeable::Resource
 
-    attributes :reference, :carrier_id, :delivery_contact_phone, :delivery_contact_email, :pick_up_at
-    writeable_attributes :carrier_id, :delivery_contact_phone, :delivery_contact_email, :pick_up_at
+    attributes :reference, :carrier_id, :delivery_contact_phone, :incoterm
+      :delivery_contact_email, :pick_up_at
+
+    writeable_attributes :carrier_id, :delivery_contact_phone, :inco_term
+      :delivery_contact_email, :pick_up_at
 
     singleton_associations :address
   end

d8547d3ebad99836443e4a96147217e3c7f09d7d
Christian Weyer <cw@crispymtn.com>
2019-02-20 20:59:30 +0100

    Update printery attributes

--- lib/keyline/resources/printery.rb
+++ lib/keyline/resources/printery.rb
@@ -4,10 +4,6 @@ module Keyline
 
     path_prefix :configuration
 
-    attributes :id, :name, :country_code, :default_imposing_margin, :template_access, :created_at, :updated_at,
-      :formula_for_contribution_margin_for_inhouse_costs, :formula_for_contribution_margin_for_external_costs,
-      :production_scheduling_days_in_advance, :production_scheduling_generations,
-      :production_scheduling_meet_deadline_importance, :production_scheduling_minimize_splits_importance,
-      :production_scheduling_pool_materials_importance, :production_scheduling_minimize_time_importance
+    attributes :id, :name, :country_code, :locale, :created_at, :updated_at
   end
 end

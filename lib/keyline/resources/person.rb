c79f12ca8ccec713e9b402c0cf4d9398f4af84dd
Christian Weyer <cw@crispymtn.com>
2019-02-21 10:05:05 +0100

    Allow reading of organization_id for persons

--- lib/keyline/resources/person.rb
+++ lib/keyline/resources/person.rb
@@ -5,13 +5,13 @@ module Keyline
 
     path_prefix :customer_relations, overwrite_parent_path: false
 
-    attributes :salutation, :first_name, :name, :reference, :email, :debitor_identifier,
-      :creditor_identifier, :preferred_locale, :tax_identifier,
-      :phone, :fax, :mobile, :credit_limit, :deleted_at
+    attributes :organization_id, :salutation, :first_name, :name, :reference,
+      :email, :debitor_identifier, :creditor_identifier, :preferred_locale,
+      :tax_identifier, :phone, :fax, :mobile, :credit_limit, :deleted_at
 
-    writeable_attributes :salutation, :first_name, :name, :reference, :email,
-      :debitor_identifier, :creditor_identifier, :preferred_locale, :tax_identifier,
-      :phone, :fax, :mobile, :credit_limit
+    writeable_attributes :organization_id,  :salutation, :first_name, :name,
+      :reference, :email, :debitor_identifier, :creditor_identifier, :preferred_locale,
+      :tax_identifier, :phone, :fax, :mobile, :credit_limit
 
     # Provides a possiblity for the given resource
     # to overwrite paths based on HTTP verb
--- lib/keyline/version.rb
+++ lib/keyline/version.rb
@@ -1,3 +1,3 @@
 module Keyline
-  VERSION = "1.4.14"
+  VERSION = "1.4.15"
 end

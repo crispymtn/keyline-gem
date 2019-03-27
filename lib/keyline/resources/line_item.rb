de45a19839279af9f8a793378959740e84ce3f6b
Sascha Weidlich <sascha.weidlich@mac.com>
2019-03-27 17:04:32 +0100

    Update attributes for line items

--- lib/keyline/resources/line_item.rb
+++ lib/keyline/resources/line_item.rb
@@ -2,8 +2,9 @@ module Keyline
   class LineItem
     include Jeweler::Resource
 
-    attributes :description, :qty, :unit, :net_total, :gross_total, :net,
-    :discount, :discounted_net_total, :order_number, :environmental_certifications
+    attributes :description, :qty, :unit, :net_total, :tax_rate, :gross_total,
+      :net, :discount, :discounted_net_total, :order_number,
+      :environmental_certifications
 
     singleton_associations :accounting_category
   end

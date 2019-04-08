ce359ee57ccaf8cad573d578426ae83a4d40fe2c
Christian Weyer <cw@crispymtn.com>
2019-04-08 11:47:31 +0200

    Allow keep_grain_when_precutting on imposings to be set and read

--- lib/keyline/resources/imposing.rb
+++ lib/keyline/resources/imposing.rb
@@ -7,13 +7,15 @@ module Keyline
       :maximum_number_of_mups_per_sheet, :imposing_mode, :cutting, :cropping,
       :cropping_margin, :connected_mups_after_cutting, :default_margin,
       :default_stock_folding_pattern_id, :auto_signature_generation,
-      :allow_printing_on_gripper, :disable_folding, :mup_orientation
+      :allow_printing_on_gripper, :disable_folding, :mup_orientation,
+      :keep_grain_when_precutting
 
     writeable_attributes :printing_technique,
       :maximum_number_of_mups_per_sheet, :imposing_mode, :cutting, :cropping,
       :cropping_margin, :connected_mups_after_cutting, :default_margin,
       :default_stock_folding_pattern_id, :auto_signature_generation,
-      :allow_printing_on_gripper, :disable_folding, :mup_orientation
+      :allow_printing_on_gripper, :disable_folding, :mup_orientation,
+      :keep_grain_when_precutting
 
     associations :master_signatures
     singleton_associations :substrate
--- lib/keyline/version.rb
+++ lib/keyline/version.rb
@@ -1,3 +1,3 @@
 module Keyline
-  VERSION = "1.5.7"
+  VERSION = "1.5.8"
 end

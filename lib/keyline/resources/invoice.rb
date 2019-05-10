924c0a1d461937c5341c66957e03c343b0217f03
Sascha Weidlich <sascha.weidlich@mac.com>
2019-05-10 12:06:41 +0200

    Update attrributes for invoices and customer invoices

--- lib/keyline/resources/invoice.rb
+++ lib/keyline/resources/invoice.rb
@@ -6,7 +6,7 @@ module Keyline
 
     attributes :recipient_id, :recipient_type, :business_unit_id, :reversed_invoice_id,
       :number, :net_total, :gross_total, :paid_at, :custom_text, :taxes, :due_at,
-      :billed_at, :sent_at, :custom_references, :services_performed_at, :type
+      :billed_at, :sent_at, :custom_references, :services_performed_at, :type, :canceled_at
 
     associations :line_items
     singleton_associations :address, :recipient, :contact, :payment_terms

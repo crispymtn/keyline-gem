cbe06c1b955b7688e04c7c37456352a3f6e029d3
Sascha Weidlich <sascha.weidlich@mac.com>
2019-06-03 12:01:39 +0200

    Update attrributes for production tasks

--- lib/keyline/resources/production/task.rb
+++ lib/keyline/resources/production/task.rb
@@ -8,7 +8,9 @@ module Keyline::Production
       :outsourced, :inline, :checkpoint, :duration_for_preparations,
       :duration_for_execution, :duration_for_cleanup, :duration_for_pause_until_next_task,
       :manufactured_product, :product_processing, :amount_of_wasted_manufactured_products,
-      :minimum_viable_amount_of_manufactured_products, :amount_of_manufactured_products
+      :minimum_viable_amount_of_manufactured_products, :amount_of_manufactured_products,
+      :actual_duration_for_preparations, :actual_duration_for_execution, :actual_duration_for_cleanup,
+      :product_id
 
     associations :material_demands, :task_executions
     singleton_associations :means_of_production

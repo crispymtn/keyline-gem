0c6b07005897798d2ee0ab503f5b2c2938ab703b
Sascha Weidlich <sascha.weidlich@mac.com>
2019-07-05 09:22:32 +0200

    Update attrributes for address and shipment

--- lib/keyline/resources/address.rb
+++ lib/keyline/resources/address.rb
@@ -4,8 +4,8 @@ module Keyline
     include Jeweler::Writeable::Resource
 
     attributes :addressable_id, :addressable_type, :addressee, :street, :number,
-      :zip_code, :town, :country_code, :state_or_province, :fingerprint
-      
-    writeable_attributes :addressee, :street, :number, :zip_code, :town, :country_code, :state_or_province
+      :addition, :zip_code, :town, :country_code, :state_or_province, :fingerprint
+
+    writeable_attributes :addressee, :street, :number, :addition, :zip_code, :town, :country_code, :state_or_province
   end
 end
--- lib/keyline/resources/shipment.rb
+++ lib/keyline/resources/shipment.rb
@@ -4,10 +4,10 @@ module Keyline
     include Jeweler::Writeable::Resource
 
     attributes :reference, :carrier_id, :delivery_contact_phone, :incoterm,
-      :delivery_contact_email, :pick_up_at
+      :delivery_contact_email, :pick_up_at, :delivery_instructions
 
     writeable_attributes :carrier_id, :delivery_contact_phone, :incoterm,
-      :delivery_contact_email, :pick_up_at
+      :delivery_contact_email, :pick_up_at, :delivery_instructions
 
     singleton_associations :address, :origin_address, :carrier
     associations :document_callbacks, :parcels

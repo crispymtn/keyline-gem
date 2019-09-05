9dce74344f7694cc06966baa67d9d7809e238c1c
Sascha Weidlich <sascha.weidlich@mac.com>
2019-09-05 12:59:47 +0200

    Update attrributes for print data files

--- lib/keyline/resources/print_data_file.rb
+++ lib/keyline/resources/print_data_file.rb
@@ -3,8 +3,13 @@ module Keyline
     include Jeweler::Resource
     include Jeweler::Writeable::Resource
 
-    attributes :product_id, :component_id, :kind, :state, :progress, :uri, :preview_path, :filesize, :first_page, :last_page
-    writeable_attributes :kind, :state, :progress, :uri, :preview_path, :filesize, :first_page, :last_page
+    attributes :product_id, :component_id, :kind, :state, :progress, :uri,
+    :preview_path, :filesize, :first_page, :last_page, :review_url,
+    :approved_at
+
+    writeable_attributes :kind, :state, :progress, :uri, :preview_path,
+    :filesize, :first_page, :last_page, :review_url, :approved_at
+
     associations :print_data_errata
   end
 end
--- lib/keyline/resources/production/print_data_file.rb
+++ lib/keyline/resources/production/print_data_file.rb
@@ -3,7 +3,8 @@ module Keyline::Production
     include Jeweler::Resource
 
     attributes :printable_id, :printable_type, :kind, :state, :progress, :uri,
-      :preview_path, :filesize, :first_page, :last_page
+      :preview_path, :filesize, :first_page, :last_page, :review_url,
+      :approved_at
       
     associations :print_data_errata
   end

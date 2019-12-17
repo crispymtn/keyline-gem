b1c789a109e96ea16c542c19aeb14254fa82398e
Sascha Weidlich <sascha.weidlich@mac.com>
2019-12-17 14:26:43 +0100

    Update shipping_costs to production path in production

--- lib/keyline/resources/production/selected_production_path.rb
+++ lib/keyline/resources/production/selected_production_path.rb
@@ -8,7 +8,7 @@ module Keyline::Production
       :show_in_offer, :production_costs, :costs_per_unit, :costs_per_thousand, :total,
       :margin, :discount, :discounted_total, :outsourced, :keep_total,
       :contribution_margin_for_inhouse_costs, :contribution_margin_for_external_costs, :margin_stepping,
-      :material_costs
+      :material_costs, :shipping_costs
 
     associations :tasks
   end

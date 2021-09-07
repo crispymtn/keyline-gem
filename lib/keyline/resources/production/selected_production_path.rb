7e3f57a9d5ac85a1056ac758159b32013ec40140
Sascha Weidlich <sascha.weidlich@mac.com>
2021-09-07 13:45:09 +0200

    Remove obsolete margin contributions and margin stepping from production path

--- lib/keyline/resources/production/selected_production_path.rb
+++ lib/keyline/resources/production/selected_production_path.rb
@@ -7,7 +7,6 @@ module Keyline::Production
     attributes :state, :circulation, :selected_for_production,
       :show_in_offer, :production_costs, :costs_per_unit, :costs_per_thousand, :total,
       :margin, :discount, :discounted_total, :outsourced, :keep_total,
-      :contribution_margin_for_inhouse_costs, :contribution_margin_for_external_costs, :margin_stepping,
       :material_costs, :shipping_costs
 
     associations :tasks
--- lib/keyline/resources/production_path.rb
+++ lib/keyline/resources/production_path.rb
@@ -6,11 +6,10 @@ module Keyline
     attributes :state, :circulation, :selected_for_production,
       :show_in_offer, :production_costs, :costs_per_unit, :costs_per_thousand, :total,
       :margin, :discount, :discounted_total, :outsourced, :keep_total,
-      :contribution_margin_for_inhouse_costs, :contribution_margin_for_external_costs, :margin_stepping,
       :material_costs, :shipping_costs, :weight_calculation_mode, :product_weight
 
     writeable_attributes :selected_for_production, :show_in_offer, :total, :margin, :discount, :outsourced,
-      :keep_total, :margin_stepping, :weight_calculation_mode, :product_weight
+      :keep_total, :weight_calculation_mode, :product_weight
 
     associations :circulations, :production_flow_modifications, :tasks
 

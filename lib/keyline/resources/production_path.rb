1dfa4409ead6e28bd5240e919c6a5400d7310706
Sascha Weidlich <sascha.weidlich@mac.com>
2021-09-21 23:16:51 +0200

    Update attributes for production path

--- lib/keyline/resources/production_path.rb
+++ lib/keyline/resources/production_path.rb
@@ -4,12 +4,12 @@ module Keyline
     include Jeweler::Writeable::Resource
 
     attributes :state, :circulation, :selected_for_production,
-      :show_in_offer, :production_costs, :costs_per_unit, :costs_per_thousand, :total,
+      :show_in_offer, :production_costs, :costs_per_unit, :costs_per_thousand, :sub_total, :total,
       :margin, :discount, :discounted_total, :outsourced, :keep_total,
       :material_costs, :shipping_costs, :weight_calculation_mode, :product_weight,
       :inhouse_costs, :external_costs
 
-    writeable_attributes :selected_for_production, :show_in_offer, :total, :margin, :discount, :outsourced,
+    writeable_attributes :selected_for_production, :show_in_offer, :sub_total, :margin, :discount, :outsourced,
       :keep_total, :weight_calculation_mode, :product_weight
 
     associations :circulations, :production_flow_modifications, :tasks

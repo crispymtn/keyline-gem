d5d21bd5dbed5b9a39f5569c751091134468a660
Sascha Weidlich <sascha.weidlich@mac.com>
2023-04-05 10:00:48 +0200

    Update imposing.rb
    Add `signature_repeat_mode` attribute to imposing

--- lib/keyline/resources/imposing.rb
+++ lib/keyline/resources/imposing.rb
@@ -8,14 +8,14 @@ module Keyline
       :cropping_margin, :connected_mups_after_cutting, :default_margin,
       :default_stock_folding_pattern_id, :auto_signature_generation,
       :allow_printing_on_gripper, :disable_folding, :mup_orientation,
-      :keep_grain_when_precutting
+      :keep_grain_when_precutting, :signature_repeat_mode
 
     writeable_attributes :printing_technique,
       :maximum_number_of_mups_per_sheet, :imposing_mode, :cutting, :cropping,
       :cropping_margin, :connected_mups_after_cutting, :default_margin,
       :default_stock_folding_pattern_id, :auto_signature_generation,
       :allow_printing_on_gripper, :disable_folding, :mup_orientation,
-      :keep_grain_when_precutting
+      :keep_grain_when_precutting, :signature_repeat_mode
 
     associations :master_signatures, :sheets
     singleton_associations :substrate
